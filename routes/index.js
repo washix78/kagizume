c7b7592a9ed1ccbfd099679ffe0b409d5961d38b
washix78 <moritzmoretz26@gmail.com>
2019-03-16 21:07:01 +0900

    service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
+'use strict';
+
+var config = require('config');
 var express = require('express');
 var router = express.Router();
 
+var services = require('../services');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/dir', (req, res, next) => {
+  try {
+    var dpaths = services.listDpaths(config.rootDpath);
+    res.json(dpaths);
+  } catch (e) {
+    res.status(400).json({
+      message: e.message
+    });
+  }
 });
+
+router.get('/file', (req, res, next) => {
+  try {
+    var fpaths = services.listFpaths();
+    res.json(fpaths);
+  } catch (e) {
+    res.status(400).json({
+      message: e.message
+    });
+  }
+});
+
+router.put('/file', (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+});
+
+router.delete('/file', (req, res, next) => {
+  try {
+
+  } catch (e) {
+
+  }
+});
+
+
+/*
+ * /dir GET
+ * /file GET
+ * /file PUT
+ * /file DELETE
+ */
 
 module.exports = router;
---
+++ services.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var fs = require('fs');
+var log4js = require('log4js');
+var path = require('path');
+var trash = require('trash');
+
+log4js.configure('./config/log4js.json');
+var logger = log4js.getLogger('default');
+
+module.exports.listDpaths = (rootDpath) => {
+  if (!fs.existsSync(rootDpath) || !fs.statSync(rootDpath).isDirectory()) {
+    var message = `Not directory ${rootDpath}.`;
+    logger.error(message);
+    throw new Error(message);
+  }
+  var allDpaths = [];
+
+  var loop = (topDpath) => {
+    var paths = fs.readdirSync(topDpath).map((name) => {
+      return path.resolve(topDpath, name);
+    }).filter((test) => {
+      return fs.statSync(test).isDirectory();
+    });
+    Array.prototype.push.apply(allDpaths, paths);
+  };
+  loop(rootDpath);
+
+  return allDpaths;
+};
+
+module.exports.listFpaths = (dpath) => {
+  var fpaths = fs.readdirSync(dpath).map((name) => {
+    return path.resolve(dpath, name);
+  }).filter((test) => {
+    return !fs.statSync(test).isDirectory();
+  });
+  return fpaths;
+};
+
+module.exports.removeFpaths = (fpaths) => {
+  if (!Array.isArray(fpaths)) {
+    throw new Error('Not array.');
+  }
+
+  fpaths.forEach((fpath) => {
+    if (fs.statSync(fpath).isDirectory()) {
+      throw new Error(`${fpath} is directory.`);
+    }
+  });
+
+  trash(fpaths).then(() => {
+    return true;
+  }).catch((e) => {
+    logger.error(e.stack);
+    return e;
+  });
+};
+
+module.exports.moveFpaths = (apaths, bpaths) => {
+  if (!Array.isArray(apaths) || !Array.isArray(bpaths) || apaths.length !== bpaths.length) {
+    throw new Error('Illegal arrays.');
+  }
+
+  apaths.forEach((fpath) => {
+    if (!fs.existsSync(fpath)) {
+      throw new Error(`${fpath} not exist.`);
+    }
+    if (fs.statSync(fpath).isDirectory()) {
+      throw new Error(`${fpath} is directory.`);
+    }
+
+    bpaths.forEach((fpath) => {
+      if (fs.existsSync(fpath)) {
+        throw new Error(`${fpath} exists.`);
+      }
+
+      if (bPaths.indexOf(fpath) !== -1) {
+        throw new Error(`Duplicate path ${fpath}.`);
+      }
+    });
+  });
+
+  for (var i = 0; i < apaths.length; i++) {
+    var apath = apaths[i];
+    var bpath = bpaths[i];
+    if (path.dirname(apath) !== path.dirname(bpath)) {
+      throw new Error(`Not same directory. ${apath} and ${bpath}.`);
+    }
+  }
+
+  var errors = [];
+  for (var i = 0; i < apaths.length; i++) {
+    var apath = apaths[i];
+    var bpath = bpaths[i];
+    try {
+      fs.renameSync(apath, bpath);
+    } catch (e) {
+      errors.push(e);
+    }
+  }
+};
